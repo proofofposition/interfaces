8914d3844c951378bcb5eacb1ab817140b1e5818
Gareth Edwards <garrethedwards@gmail.com>
2022-12-12 15:35:22 +0200

    Tweaks

--- IJobNFT.sol
+++ IJobNFT.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/interfaces/IERC721.sol";
 
 interface IJobNFT is IERC721{
-    function canMintJob(string calldata uri, address minter) external view returns (bool);
+    function canMintJob(string memory _uri, address _minter, uint32 _employerTokenId ) external view returns (bool);
     function approveMint(address employee, string memory uri) external;
-    function mintItem(address employee) external;
+    function mintItem() external;
     function getEmployerIdFromJobId(uint256 _jobId) external view returns (uint32);
     function getJobIdFromEmployee(address _employee) external view returns (uint256);
 }
