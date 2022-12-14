9a727d07f688c52b507d5f159c0299ab539d5ce6
Gareth Edwards <garrethedwards@gmail.com>
2022-12-14 16:45:03 +0200

    Update mintFor

--- IJobNFT.sol
+++ IJobNFT.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/interfaces/IERC721.sol";
 interface IJobNFT is IERC721{
     function canMintJob(string memory _uri, address _minter, uint32 _employerTokenId) external view returns (bool);
     function approveMint(address employee, string memory uri) external;
-    function mintFor(address employee) external;
+    function mintFor(address employee, uint32 employerId) external;
     function getEmployerIdFromJobId(uint256 _jobId) external view returns (uint32);
     function getJobIdFromEmployee(address _employee) external view returns (uint256);
 }
