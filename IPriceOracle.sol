7c073c45e64497262a73401ce5cc5bcd8aa20ae5
Gareth Edwards <garrethedwards@gmail.com>
2023-01-22 08:40:00 +0200

    Cents to token

--- IPriceOracle.sol
+++ IPriceOracle.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.8.0;
 
 interface IPriceOracle {
     function getPrice() external view returns (uint16);
+    function centsToToken(uint256 _usdCents) external view returns (uint256);
 }
