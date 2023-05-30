42bae3f36d1fa2a626f5f32c2da0e681e1cefe5d
Gareth Edwards <garrethedwards@gmail.com>
2023-05-30 14:33:38 +0200

    Remove interface method

--- IAccessCardSft.sol
+++ IAccessCardSft.sol
@@ -3,6 +3,4 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/interfaces/IERC1155.sol";
 
-interface IAccessCardSft is IERC1155{
-    function employerIdFromWallet(address _address) external view returns (uint32);
-}
+interface IAccessCardSft is IERC1155{}
