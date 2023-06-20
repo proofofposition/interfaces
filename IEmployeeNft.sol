3999213c54615a2d0b801427fc91a0ecf96b76d7
Gareth Edwards <garrethedwards@gmail.com>
2023-06-20 10:37:39 +0200

    refactor method name `getEmployerId`

--- IEmployeeNft.sol
+++ IEmployeeNft.sol
@@ -5,5 +5,5 @@ import "@openzeppelin/contracts-upgradeable/interfaces/IERC721Upgradeable.sol";
 
 interface IEmployeeNft is IERC721Upgradeable{
     function mintFor(address _employee, uint32 _employerId) external;
-    function getEmployerIdFromJobId(uint256 _jobId) external view returns (uint32);
+    function getEmployerId(uint256 _jobId) external view returns (uint32);
 }
