dc3c5dc2a3cf36fd5d07a5764bb460f1e204464a
Gareth Edwards <garrethedwards@gmail.com>
2023-08-08 09:54:05 +0200

    refactor to employer key

--- IEmployerSft.sol
+++ IEmployerSft.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts-upgradeable/interfaces/IERC1155Upgradeable.sol";
 
 interface IEmployerSft is IERC1155Upgradeable{
-    function employerIdFromWallet(address _address) external view returns (uint32);
+    function employerKeyFromWallet(address _address) external view returns (string memory);
     function addToMyTeam(address _to) external returns (uint256);
     function removeFromMyTeam(address _from, uint32 _timestamp) external;
     function invalidFrom(address _address) external view returns (uint32);
